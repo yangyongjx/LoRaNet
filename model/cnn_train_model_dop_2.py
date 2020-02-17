0c6e809e0b0a089e72f2085da0f49406443b215a
Nakorn Kumchaiseemak <46889130+shikuzen@users.noreply.github.com>
2020-02-18 00:57:28 +0700

    Add files via upload

--- model/cnn_train_model_dop_2.py
+++ model/cnn_train_model_dop_2.py
@@ -1,47 +1,53 @@
 import tensorflow as tf
 import os
-import cv2
+# import cv2
 import numpy as np
 import re
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+# import matplotlib.pyplot as plt
+# from matplotlib.animation import FuncAnimation
 from tensorflow.compat.v1 import ConfigProto
 import collections
+from sklearn.model_selection import train_test_split
+import tensorflow.python.framework.dtypes
+sss
+os.environ["CUDA_VISIBLE_DEVICES"]="2"
 
 def pre_process():
-    label1 = np.load("D:/NestData/10-5-2019-64-chirp-16bit/pos_process_new/radar_pos_label_11-36_deleted.npy")
-    label2 = np.load("D:/NestData/10-5-2019-64-chirp-16bit/pos_process_new/radar_pos_label_37-59_deleted.npy")
-    deleted1 = np.load("D:/NestData/10-5-2019-64-chirp-16bit/pos_process_new/deleted_index_11-36.npy")
-    deleted2 = np.load("D:/NestData/10-5-2019-64-chirp-16bit/pos_process_new/deleted_index_37-59.npy")
-    deleted2 += 11664
-    # print(deleted1.shape, deleted2.shape)
-    deletedCom = np.concatenate((deleted1,deleted2))
+    # dis_label = np.load("/home/nakorn/data/radar_pos_label_deleted.npy")
+    # dis_label = np.load("D:/NestData/3tx-32chirp-jaco-55times_all/data_usage/radar_pos_label_deleted.npy")
+    # deleted = np.load("D:/NestData/3tx-32chirp-jaco-55times_all/data_usage/deleted_index.npy")   
+    # print(dis_label.shape)
+    # deleted = np.load("/home/nakorn/data/deleted_index.npy")   
+    # dis_label = dis_label[:,0,:]
+    # radar_data = 1
+    radar_data = np.load("D:/NestData/3tx-32chirp-jaco-55times_all/data_usage/radar_data_all_real_imag_new_cut.npy")
+    
+    deleted = np.load("D:/NestData/3tx-32chirp-jaco-55times_all/data_usage/deleted_index.npy")
+    dis_label = np.load("D:/NestData/3tx-32chirp-jaco-55times_all/data_usage/radar_pos_label_deleted.npy")
+
     
 
-    radar_data = np.load("D:/NestData/10-5-2019-64-chirp-16bit/pos_process_new/radar_data_reduce/radar_data_reduce_all_real_imag.npy")
-    print(radar_data.shape)
-    radar_data = np.swapaxes(np.swapaxes(radar_data, 1,2),2,3)
-    # radar_data = radar_data[:,:,0,:]
-    radar_data = np.delete(radar_data, deletedCom, axis =0)
+    print(radar_data.shape, dis_label.shape)
+    
+    radar_data = np.delete(radar_data, deleted, axis=0)
 
-    radar_test_data = np.concatenate((radar_data[:486], radar_data[6317:6803], radar_data[12636:13122], radar_data[16038:16524], radar_data[18468:18954]))
-    # radar_test_data = np.concatenate((radar_data[6317:6803], radar_data[12636:13122], radar_data[16038:16524], radar_data[18468:18954]))
-    radar_train_data = np.concatenate((radar_data[486:6317], radar_data[6803:12636], radar_data[13122:16038], radar_data[16524:18468], radar_data[18954:]))
-    # radar_train_data = np.concatenate((radar_data[:6317], radar_data[6803:12636], radar_data[13122:16038], radar_data[16524:18468], radar_data[18954:]))
-    print(radar_test_data.shape, radar_train_data.shape)
-
-    dis_label = np.concatenate((label1, label2))
     dis_label = dis_label[:,0,:]
-    # print(dis_label.shape)
+    
+    radar_data = radar_data[:5000]
+    dis_label = dis_label[:5000]
+    
+    print(radar_data.shape, dis_label.shape)
+   
+    radar_data = np.float32(radar_data)
+    dis_label = np.float32(dis_label)
 
-    dis_test_label = np.concatenate((dis_label[:486], dis_label[6317:6803], dis_label[12636:13122], dis_label[16038:16524], dis_label[18468:18954]))
-    # dis_test_label = np.concatenate((dis_label[6317:6803], dis_label[12636:13122], dis_label[16038:16524], dis_label[18468:18954]))
-    dis_train_label = np.concatenate((dis_label[486:6317], dis_label[6803:12636], dis_label[13122:16038], dis_label[16524:18468], dis_label[18954:]))
-    # dis_train_label = np.concatenate((dis_label[:6317], dis_label[6803:12636], dis_label[13122:16038], dis_label[16524:18468], dis_label[18954:]))
-    print(dis_test_label.shape, dis_train_label.shape)
-    # print("radar_data_shape = " ,radar_data.shape, "label_data_shape = ", dis_label.shape)
+   
+
+    radar_train_data, radar_test_data, dis_train_label, dis_test_label = train_test_split(radar_data, dis_label, test_size=0.3)
+        
     
     return radar_train_data, radar_test_data, dis_train_label, dis_test_label
+  
 
 
 def fully_connected_layer(input1, input_channel, output_channel, name = "fc"):
@@ -77,21 +83,21 @@ def run_graph(radar_train_data, radar_test_data, dis_train_label, dis_test_label
     starter_learning_rate = 0.01
     
     training_epoch = 10000
-    batch_train_size = 1500
-    batch_test_size = 2430
+    batch_train_size = 500
+    batch_test_size = 500
     batch_test_len = 0
 
-    log_dir = './summary_raw_data/summary_cnn_6_layers_complex_shuffle_4/2dcov2'
-    model_path = './saved_model/model_cnn_6_layers_complex_shuffle_4/4plot.ckpt'
+    log_dir = './summary_raw_data/summarize_jaco_data_test/2dcov2'
+    model_path = './saved_model/model_jaco_data_test/4plot.ckpt'
     # model_load = './saved_model/model_cnn_6_layers_complex_1_2d/4plot.ckpt'
 
     training_phase = tf.placeholder(tf.bool, name = 'training_phase')
-
-    train_data, train_label = tf.train.shuffle_batch([radar_train_data, dis_train_label], enqueue_many=True, batch_size=batch_train_size, 
-        capacity=3000, min_after_dequeue=500, allow_smaller_final_batch = True)
-    test_data, test_label = tf.train.batch([radar_test_data, dis_test_label], enqueue_many=True, batch_size=batch_test_size, 
+    print(type(radar_train_data[0,0,0,0]), type(dis_test_label[0,0]))
+    train_data, train_label = tf.compat.v1.train.shuffle_batch([radar_train_data, dis_train_label], enqueue_many=True, batch_size=batch_train_size, 
+        capacity=int(batch_train_size*2), min_after_dequeue=int(batch_train_size/3), allow_smaller_final_batch = True)
+    test_data, test_label = tf.compat.v1.train.batch([radar_test_data, dis_test_label], enqueue_many=True, batch_size=batch_test_size, 
         capacity=batch_test_size, allow_smaller_final_batch=True)
-
+    print("ddd")
     x_p, y_p =  tf.cond(training_phase, lambda: (train_data, train_label) , lambda: (test_data, test_label))
     # x_p = tf.placeholder(tf.float32, shape=[None, 20, 20, 8], name= 'x_train')
     # y_p = tf.placeholder(tf.float32, shape=[None, 3], name= 'y_train')
@@ -99,31 +105,31 @@ def run_graph(radar_train_data, radar_test_data, dis_train_label, dis_test_label
     y_p = tf.cast(y_p, dtype=tf.float32)
 
     print(x_p)
-    conv2d = conv2d_layer(x_p, 1, 3, 8, 4, "conv2_layer1_en")
-    conv2d = conv2d_layer(conv2d, 2, 3, 4, 4, "conv2_layer2_en")
+    conv2d = conv2d_layer(x_p, 1, 3, 24, 8, "conv2_layer1_en")
+    conv2d = conv2d_layer(conv2d, 2, 3, 8, 16, "conv2_layer2_en")
     # conv2d = tf.contrib.layers.batch_norm(conv2d, is_training = training_phase) 
 
-    conv2d = conv2d_layer(conv2d, 1 , 3, 4, 8, "conv2_layer3_en")
-    conv2d = conv2d_layer(conv2d, 2 , 3, 8, 8, "conv2_layer4_en")
+    conv2d = conv2d_layer(conv2d, 1 , 3, 16, 16, "conv2_layer3_en")
+    conv2d = conv2d_layer(conv2d, 2 , 3, 16, 32, "conv2_layer4_en")
     # conv2d = tf.contrib.layers.batch_norm(conv2d, is_training = training_phase)
 
-    conv2d = conv2d_layer(conv2d, 1 , 3, 8, 16, "conv2_layer5_en")
-    conv2d = conv2d_layer(conv2d, 2 , 3, 16, 16, "conv2_layer6_en")
+    conv2d = conv2d_layer(conv2d, 1 , 3, 32, 32, "conv2_layer5_en")
+    conv2d = conv2d_layer(conv2d, 2 , 3, 32, 64, "conv2_layer6_en")
     # conv2d = tf.contrib.layers.batch_norm(conv2d, is_training = training_phase)
 
-    # conv1d = conv2d_layer(conv1d, 1 , 3, 32, 32, "conv1_layer7_en", "leaky")
-    # conv1d = conv2d_layer(conv1d, 2 , 3, 32, 64, "conv1_layer8_en", "leaky")    
+    # conv2d = conv2d_layer(conv2d, 1 , 3, 32, 32, "conv1_layer7_en")
+    # conv2d = conv2d_layer(conv2d, 2 , 3, 32, 64, "conv1_layer8_en")    
 
-    flatten = tf.reshape(conv2d, [-1, 4*4*16 ])
+    flatten = tf.reshape(conv2d, [-1, 4*4*64 ])
 
-    dense1, none = fully_connected_layer(flatten, 4*4*16, 200, "fc1")
+    dense1, none = fully_connected_layer(flatten, 4*4*64, 200, "fc1")
     # dense1 = tf.contrib.layers.batch_norm(dense1, is_training = training_phase)
     none, denseOut  = fully_connected_layer(dense1, 200, 3, "fc2")
     # denseOut= fully_connected_layer(dense2, 200, 3, "fcOut", "None")
     loss, hist = L2_loss(denseOut, y_p)
 
 
-    learning_rate = tf.compat.v1.train.exponential_decay(starter_learning_rate, global_step, 1200, 0.96, staircase=True)
+    learning_rate = tf.compat.v1.train.exponential_decay(starter_learning_rate, global_step, 5000, 0.96, staircase=True)
 
     train_optimiser = tf.train.AdamOptimizer(learning_rate=learning_rate)
     
@@ -139,8 +145,10 @@ def run_graph(radar_train_data, radar_test_data, dis_train_label, dis_test_label
     saver = tf.train.Saver()
 
     config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
+    # config.gpu_options.allow_growth = True
 
+    
+        
     with tf.Session(config=config) as sess:
         
         sess.run(init)
@@ -194,7 +202,7 @@ def run_graph(radar_train_data, radar_test_data, dis_train_label, dis_test_label
 
             writer2.add_summary(histrogram,epoch)
 
-           
+        
             '''
             saving model path
             '''
@@ -206,7 +214,7 @@ def run_graph(radar_train_data, radar_test_data, dis_train_label, dis_test_label
                     print("Last_test_acc =", np.array(test_acc_prv)[0], "Current_test_acc", np.array(test_acc))
                     save_path = saver.save(sess, model_path)
                     print("Model saved in file: %s" %save_path)
-        
+    
         coord.request_stop()
         coord.join(threads)
 
@@ -215,15 +223,10 @@ def run_graph(radar_train_data, radar_test_data, dis_train_label, dis_test_label
 
 def main():
     radar_train_data, radar_test_data, dis_train_label, dis_test_label = pre_process()
-    # plt.imshow(radar_train_data[0,:,:,0])
-    # plt.show()
-    radar_train_data = radar_train_data[:3800]
-    dis_train_label = dis_train_label[:3800]
-    radar_test_data = radar_test_data[:600]
-    dis_test_label = dis_test_label[:600]
-    print("shape new", radar_train_data.shape, radar_test_data.shape, dis_train_label.shape, dis_test_label.shape)
+    print(radar_train_data.shape, radar_test_data.shape, dis_train_label.shape, dis_test_label.shape)
+    run_graph(radar_train_data, radar_test_data, dis_train_label, dis_test_label)
+   
 
-    # run_graph(radar_train_data, radar_test_data, dis_train_label, dis_test_label)
 
 if __name__ == "__main__":
     main()
